c17aa590fb8af444c5069798f028ef1dd3e2f597
Caio Mantovani Borba <caiovmborba@gmail.com>
2024-02-22 15:18:04 -0300

    feat: create login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,84 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useNavigate } from "react-router-dom"
+
+const FormSchema = z.object({
+  username: z.string().min(6, {
+    message: "Informe seu usuário.",
+  }),
+  password: z.string().min(6, {
+    message: "Informe sua senha.",
+  }),
+})
+
 export default function Login() {
+  const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    if (data.username === 'CaioMantovaniBorba' && data.password === '123456') {
+      return navigate("/dashboard");
+    }
+  }
+
   return (
-    <h1>Login</h1>
-  );
+    <div className="background h-screen w-screen flex items-center justify-center">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-1/3 space-y-6 items-center">
+
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Usuário</FormLabel>
+                <FormControl>
+                  <Input placeholder="" max={12} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input placeholder="" max={12} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="w-full flex justify-center">
+            <Button type="submit" className="w-1/3">Entrar</Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes as Router, Route } from 'react-router-dom';
 
 import Login from '@/pages/Login';
+import Dashboard from '@/pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Router>
     <Route path="/" Component={Login} />
+    <Route path="/dashboard" Component={Dashboard} />
   </Router>
 );
 
